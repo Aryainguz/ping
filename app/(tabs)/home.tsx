36c86429b235da6cf1514f67bc3bf0fac06d66da
Aryainguz <aryaninguz369@gmail.com>
2024-05-25 01:07:06 +0530

    feat: added auth and dark mode theme support

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useAuth } from '@clerk/clerk-expo';
 
 const home = () => {
+  const { signOut } = useAuth();
+
   return (
     <View>
       <Text>home</Text>
+
+      <Button
+        title='logout'
+        onPress={() => {
+          // Sign out the user
+          signOut();
+        }}
+      />
     </View>
   )
 }
